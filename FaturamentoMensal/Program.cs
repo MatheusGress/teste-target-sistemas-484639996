bd1a496c0a73e28cb0ebbbb11e469fefa6b3dca1
Matheus Gress <matheusaugustogress@gmail.com>
2024-09-11 01:30:02 -0300

    fix: remove special character from namespace in FaturamentoMensal

--- FaturamentoMensal/Program.cs
+++ FaturamentoMensal/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace _FaturamentoMensal
+﻿namespace FaturamentoMensal
 {
     static class Program
     {
--- FaturamentoMensal/Utilities.cs
+++ FaturamentoMensal/Utilities.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace _FaturamentoMensal
+namespace FaturamentoMensal
 {
     public static class Utilities
     {
